a2c0d88340977112f00b3013f605ce38318c8fa3
mohammed youbi <mohammedyoubi01@gmail.com>
2024-12-16 00:33:16 +0100

    first commit: xml files toolbare front-end

---
+++ app/src/main/java/com/example/myapplication/lienbutton2.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import android.content.Intent; // Import Intent
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class lienbutton2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // Link the XML layout to this activity
+
+        // Find the button by its ID
+        Button buttonToPage = findViewById(R.id.meatButton);
+
+        // Set an OnClickListener for the button
+        buttonToPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start the next activity
+                Intent intent = new Intent(lienbutton2.this, MeatActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
