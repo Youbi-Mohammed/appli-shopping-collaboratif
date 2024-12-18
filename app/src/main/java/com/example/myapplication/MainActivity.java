20dc8bafa69d437400347268cd2833f401a90979
mohammed youbi <mohammedyoubi01@gmail.com>
2024-12-18 13:24:07 +0100

    first commit: xml files toolbare front-end

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,62 +1,62 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.SearchView;
-
-import androidx.annotation.NonNull;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private DrawerLayout drawerLayout;
-    private SearchView searchView;
-    private ImageButton hamburgerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // Make sure this is your layout file
+        setContentView(R.layout.activity_main); // Link to the main XML layout
 
-        // Initialize the drawer layout and navigation view
-        drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navView = findViewById(R.id.nav_view);
+        // Initialize the buttons and set click listeners
+        Button fruitVegButton = findViewById(R.id.fruitVegButton);
+        Button meatButton = findViewById(R.id.meatButton);
+        Button dairyButton = findViewById(R.id.dairyButton);
+        Button bakeryButton = findViewById(R.id.bakeryButton);
+        Button grainsButton = findViewById(R.id.grainsButton);
 
-        // Initialize the search view
-        searchView = findViewById(R.id.search_view);
-        hamburgerButton = findViewById(R.id.hamburger_button);
-
-        // Handle search view input
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        fruitVegButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onQueryTextSubmit(String query) {
-                // This is called when the user submits their search query
-                Log.d("SearchView", "Query submitted: " + query);
-                // Here you can perform a search or filter the UI based on the query
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                // This is called as the user types into the search field
-                Log.d("SearchView", "Query text changed: " + newText);
-                // You can dynamically update your content here
-                return true;
+            public void onClick(View v) {
+                // Launch the Fruits and Vegetables Activity
+                startActivity(new Intent(MainActivity.this, FruitsVegetablesActivity.class));
             }
         });
 
-        // Handle hamburger button click to open the drawer
-        hamburgerButton.setOnClickListener(new View.OnClickListener() {
+        meatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (drawerLayout != null) {
-                    drawerLayout.openDrawer(navView); // Open the navigation drawer
-                }
+                // Launch the Meat Activity
+                startActivity(new Intent(MainActivity.this, MeatActivity.class));
+            }
+        });
+
+        dairyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Launch the Dairy Products Activity
+                startActivity(new Intent(MainActivity.this, DairyActivity.class));
+            }
+        });
+
+        bakeryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Launch the Bakery Activity
+                startActivity(new Intent(MainActivity.this, BakeryActivity.class));
+            }
+        });
+
+        grainsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Launch the Grains Activity
+                startActivity(new Intent(MainActivity.this, GrainsActivity.class));
             }
         });
     }
